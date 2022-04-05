c311b611148610973212cbfb9beb0ffbf027ade1
tmfrlrkvlek <wltn102998@gmail.com>
2022-04-05 12:17:09 +0900

    ADD: 프로젝트 생성

---
+++ Soomswim/Soomswim/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Soomswim
+//
+//  Created by 이지수 on 2022/04/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
