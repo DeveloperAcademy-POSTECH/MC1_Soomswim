a5128e26cae0b80952d77b34d10d8fc5ee1b7bab
JUNY0110 <98405970+JUNY0110@users.noreply.github.com>
2022-04-08 01:02:35 +0900

    Style: 버튼 위치 변경, NavigationView Back문구 삭제
    1. 유저의 텍스트 크기 조정
    2. 게시글 저장 기능 추가 여부 확인 필요

---
+++ Soomswim/Soomswim/Eddy/SoomswimApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoomswimApp.swift
+//  Soomswim
+//
+//  Created by 이지수 on 2022/04/05.
+//
+
+import SwiftUI
+
+@main
+struct SoomswimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
