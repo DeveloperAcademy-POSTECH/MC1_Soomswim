b5b92e3042ed0ff2ec1cc408f983f4d0fd59bbd2
Tanny <admin@Adminui-MacBookPro.local>
2022-04-08 00:29:07 +0900

    feat : 로고 샘플 이미지 추가 및 로고 이미지 하단에 Custom Text 추가

--- Soomswim/Soomswim/Tanny/Login.swift
+++ Soomswim/Soomswim/Tanny/Login.swift
@@ -11,6 +11,8 @@ extension Color {
     static let mainGreen: Color = Color(red: 163/255, green: 246/255, blue: 168/255)
     static let mainMidOrange: Color = Color(red: 241/255, green: 203/255, blue: 134/255)
     static let mainOrange: Color = Color(red: 255/255, green: 87/255, blue: 0)
+    static let mainPurple: Color = Color(red: 120/255, green: 59/255, blue: 139/255)
+    static let lightGray: Color = Color(red: 240/255, green: 240/255, blue: 245/255)
 }
 
 struct Login: View {
@@ -18,7 +20,7 @@ struct Login: View {
     
     var body: some View {
         VStack {
-            WelcomeText()
+            LogoEnglish()
             LogoImage()
             UserNameField()
             LoginButton()
@@ -26,7 +28,7 @@ struct Login: View {
     }
 }
 
-struct WelcomeText: View {
+struct LogoEnglish: View {
     var body: some View {
         HStack {
             LinearGradient(gradient: Gradient(colors: [Color.mainOrange, Color.mainMidOrange, Color.mainGreen]),
@@ -50,12 +52,31 @@ struct WelcomeText: View {
 
 struct LogoImage: View {
     var body: some View {
-        return Image("Logo")
-            .resizable()
-            .aspectRatio(contentMode: .fill)
-            .frame(width: 250, height: 150)
-            .clipped()
-            .padding(.bottom, 75)
+        VStack{
+//            Text("내 마음의 안식처")
+//                .font(.callout)
+//                .foregroundColor(Color.gray)
+            Image("Logo_purple")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 250, height: 180)
+                .clipped()
+            Group {
+                Text("숨")
+                    .font(.largeTitle)
+                    .foregroundColor(Color.mainPurple)
+                    .fontWeight(.black) +
+                Text("은 ")
+                    .foregroundColor(Color.gray) +
+                Text("쉼")
+                    .font(.largeTitle)
+                    .foregroundColor(Color.mainPurple)
+                    .fontWeight(.black) +
+                Text("터")
+                    .foregroundColor(Color.gray)
+            }
+        }
+            .padding(.bottom, 30)
     }
 }
 
@@ -65,13 +86,12 @@ struct UserNameField: View {
     var body: some View {
         return TextField("Username", text: $username)
             .padding()
-            .background(Color.gray)
+            .background(Color.lightGray)
             .cornerRadius(5.0)
             .padding(.horizontal, 80)
             .padding(.vertical, 20)
     }
 }
-
 
 struct LoginButtonContent: View {
     var body: some View {
