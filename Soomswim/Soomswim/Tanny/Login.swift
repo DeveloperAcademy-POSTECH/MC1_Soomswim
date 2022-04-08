73f066775321748ecad542996e2628ca15c113ee
Tanny <xogus9641@naver.com>
2022-04-08 11:18:49 +0900

    feat: 포인트 컬러 변경 및 추가

--- Soomswim/Soomswim/Tanny/Login.swift
+++ Soomswim/Soomswim/Tanny/Login.swift
@@ -11,7 +11,8 @@ extension Color {
     static let mainGreen: Color = Color(red: 163/255, green: 246/255, blue: 168/255)
     static let mainMidOrange: Color = Color(red: 241/255, green: 203/255, blue: 134/255)
     static let mainOrange: Color = Color(red: 255/255, green: 87/255, blue: 0)
-    static let mainPurple: Color = Color(red: 120/255, green: 59/255, blue: 139/255)
+    static let subPurple: Color = Color(red: 120/255, green: 59/255, blue: 139/255)
+    static let mainPurple: Color = Color(red: 228/255, green: 198/255, blue: 255/255)
     static let lightGray: Color = Color(red: 240/255, green: 240/255, blue: 245/255)
 }
 
