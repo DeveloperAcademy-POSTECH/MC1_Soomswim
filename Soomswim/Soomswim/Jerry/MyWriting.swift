c7a7ebb55f23ae9003884fe6a5f8807b013baf22
YoonyoungL <yyunglee97@gmail.com>
2022-04-11 09:22:25 +0900

    feat: 내 페이지 뷰 수정

--- Soomswim/Soomswim/Jerry/MyWriting.swift
+++ Soomswim/Soomswim/Jerry/MyWriting.swift
@@ -26,7 +26,7 @@ struct MyWritingView: View {
             .toolbar {
                 ToolbarItemGroup(placement: .bottomBar) {
                     Spacer()
-                    NavigationLink(story.replyCheckPermission ? "답변 보기" : "답변 보기까지 \(story.remainingTime ?? "")", destination: ReplyToWritingView(color: .purple))
+                    NavigationLink(story.replyCheckPermission ? "답변 보기" : "답변 보기까지 \(story.remainingTime ?? "")", destination: ReplyToWritingView(story: story))
                         .disabled(!story.replyCheckPermission)
                 }
             }
