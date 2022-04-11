2d0adf5b162d56f91a6bdcf564bb438083c431aa
tmfrlrkvlek <wltn102998@gmail.com>
2022-04-11 23:22:54 +0900

    feat: 친구 요청 기능 완성

--- Soomswim/Soomswim/Dinner/FriendsList/FriendsListPage.swift
+++ Soomswim/Soomswim/Dinner/FriendsList/FriendsListPage.swift
@@ -11,6 +11,8 @@ struct FriendsListPage: View {
     private let viewRouter: ViewRouter
     @State private var name: String
     @ObservedObject private var friends = DataLoader<Array<User>>()
+    @State private var offer: Bool = false
+    @State private var friendName: String = ""
     
     init(name: String, viewRouter: ViewRouter) {
         self.name = name
@@ -19,43 +21,91 @@ struct FriendsListPage: View {
     }
     
     var body: some View {
-        VStack {
-            HStack {
-                Button(action: {
-                    self.viewRouter.switchPage(.mypage)
-                }) {
-                    Image(systemName: "chevron.backward")
-                        .resizable()
-                        .frame(width: 12, height: 21)
-                        .foregroundColor(Color(uiColor: UIColor.systemGray2))
+        ZStack {
+            VStack {
+                HStack {
+                    Button(action: {
+                        self.viewRouter.switchPage(.mypage)
+                    }) {
+                        Image(systemName: "chevron.backward")
+                            .resizable()
+                            .frame(width: 12, height: 21)
+                            .foregroundColor(Color(uiColor: UIColor.systemGray2))
+                    }
+                    .padding(.leading, 14)
+                    Spacer()
+                    HeaderLogo()
+                        .padding(.trailing, 12)
                 }
-                .padding(.leading, 14)
-                Spacer()
-                HeaderLogo()
-                    .padding(.trailing, 12)
-            }
-            .frame(width: nil, height: 67, alignment: .center)
-            HStack {
-                Text("친구 리스트")
-                    .font(Font.system(size: 19, weight: .semibold))
-                    .padding(.leading, 14.5)
-                Spacer()
-                Button(action: {
-                    print("search")
-                }, label: {
-                    Image(systemName: "magnifyingglass")
-                        .resizable()
-                        .foregroundColor(Color.gray)
-                        .frame(width: 23, height: 23)
-                })
-                .padding(.trailing, 14.5)
-            }
-            ScrollView {
-                if let data = self.friends.data {
-                    ForEach(data, content: { friend in
-                        FriendsList(user: friend)
+                .frame(width: nil, height: 67, alignment: .center)
+                HStack {
+                    Text("친구 리스트")
+                        .font(Font.system(size: 19, weight: .semibold))
+                        .padding(.leading, 14.5)
+                    Spacer()
+                    Button(action: {
+                        self.offer = true
+                    }, label: {
+                        Image(systemName: "plus")
+                            .resizable()
+                            .foregroundColor(Color.gray)
+                            .frame(width: 23, height: 23)
                     })
+                    .padding(.trailing, 14.5)
                 }
+                ScrollView {
+                    if let data = self.friends.data {
+                        ForEach(data, content: { friend in
+                            FriendsList(user: friend)
+                        })
+                    }
+                }
+            }
+            if self.offer {
+                ZStack {
+                    Rectangle()
+                        .shadow(color: Color.gray, radius: 5)
+                        .foregroundColor(Color.white)
+                        .frame(width: 300, height: 500, alignment: .center)
+                    VStack {
+                        HStack {
+                            Spacer()
+                            Button(action: {
+                                self.offer = false
+                            }) {
+                                Image(systemName: "xmark")
+                                    .resizable()
+                                    .foregroundColor(Color.gray)
+                                    .frame(width: 20, height: 20)
+                            }
+                            .padding(.trailing, 5)
+                            .padding(.top, 10)
+                        }
+                        .padding(.bottom, 50)
+                        Spacer()
+                        TextField("Username", text: self.$friendName)
+                            .padding(10)
+                            .frame(width: 200, height: 50, alignment: .center)
+                            .border(.gray, width: 1)
+                            .cornerRadius(10)
+                        Button(action: {
+                            self.follow()
+                        }) {
+                            ZStack {
+                                Rectangle()
+                                    .frame(width: 200, height: 50, alignment: .center)
+                                    .foregroundColor(Color.mainOrange)
+                                    .cornerRadius(10)
+                                Text("친구 요청")
+                                    .foregroundColor(Color.white)
+                                    .font(Font.system(size: 17, weight: .semibold))
+                            }
+                        }
+                        Spacer()
+                    }
+                }
+                .frame(width: 300, height: 500, alignment: .center)
+                
             }
         }
     }
@@ -71,7 +121,15 @@ struct FriendsListPage: View {
         else { return print("no contents") }
         self.friends.fill(data: friends)
     }
-                                 
+    
+    private func follow() {
+        guard let request = try? RequestFactory(url: SoomswimURL.friendRequest).request(params: ["name": self.name, "friend": self.friendName]) else { return print("error") }
+        NetworkService().request(request, handler: self.followComplete)
+    }
+    
+    private func followComplete(data: Response<String>, response: URLResponse?) {
+        self.offer = false
+    }
 }
 
 struct FriendsListPage_Previews: PreviewProvider {
