94403bcfa348ce50b1b53144266eacdb26707d56
Tanny <xogus9641@naver.com>
2022-04-12 22:12:10 +0900

    feat: 댓글 작성 팝업 창 위치 오류 수정 및 프레임 크기 조정

--- Soomswim/Soomswim/Key/Posting.swift
+++ Soomswim/Soomswim/Key/Posting.swift
@@ -11,6 +11,11 @@ struct Posting: View {
     private let story: Story2
     @ObservedObject private var detailStory: DataLoader<Story2>
     
+    // 댓글쓰기 팝업을 위한 변수
+    @State private var popUp: Bool = false
+    @State private var text: String = "여기에 댓글을 남겨주세요"
+    @State var nickname = ""
+    
     init(story: Story2) {
         self.story = story
         self.detailStory = DataLoader<Story2>()
@@ -18,26 +23,79 @@ struct Posting: View {
     }
     
     var body: some View {
-        VStack {
-            if let detailStory = self.detailStory.data {
-                ScrollView{
-                    PostingUser(name: detailStory.writer.name)
-                    Seperator()
-                    Text("\(detailStory.content)")
-                        .padding(.all, 20)
-                        .lineSpacing(10)
+        ZStack {
+            VStack {
+                if let detailStory = self.detailStory.data {
+                    ScrollView{
+                        PostingUser(name: detailStory.writer.name)
+                        Seperator()
+                        Text("\(detailStory.content)")
+                            .padding(.all, 20)
+                            .lineSpacing(10)
+                    }
                 }
+                // 수정필요: 이름 받아와야 함
+                PostingFooter(popUp: $popUp)
+                    .frame(width: nil, height: nil, alignment: .bottom)
             }
-            // 수정필요: 이름 받아와야 함
-            PostingFooter(name: "Eddy")
-                .frame(width: nil, height: nil, alignment: .bottom)
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarItems(leading: BackButton())
+            .navigationBarBackButtonHidden(true)
+            .toolbar {
+                LogoEnglish()
+            }
+            
+            if $popUp.wrappedValue {
+                ZStack {
+                    Color.white
+                    VStack {
+                        HStack {
+                            Spacer()
+                            Button(action: {
+                                // 수정필요: back-end 붙이기
+                                print("댓글 작성 clicked")
+                            }) {
+                                Text("보내기")
+                                    .foregroundColor(Color.blue)
+                            }
+                            Button(action: {
+                                self.popUp = false
+                            }) {
+                                Text("취소")
+                                    .foregroundColor(Color.black)
+                            .padding(.all, 20)
+                            }
+                        }
+                        // 수정필요: 게시글 작성자 name 넣어야 함
+                        Text("To. Eddy")
+                            .font(.system(size: 21, weight: .heavy))
+                            .frame(width: 300, height: nil, alignment: .leading)
+                        
+                        TextEditor(text: self.$text)
+                            .foregroundColor(self.text == "여기에 글을 남겨주세요" ? .gray : .primary)
+                            .onTapGesture {
+                                if self.text == "여기에 글을 남겨주세요" {
+                                    self.text = ""
+                                }
+                                    
+                            }
+                            .disableAutocorrection(true)
+                            .padding()
+                        HStack() {
+                            Spacer()
+                            Text("From.")
+                            TextField("익명", text: $nickname)
+                        }
+                        .font(.system(size: 21, weight: .heavy))
+                        .padding(.all, 20)
+                        .padding(.leading, 180)
+                    }
+                }
+                .frame(width: 360, height: 400, alignment: .center)
+                .cornerRadius(10).shadow(radius: 20)
+            }
         }
-        .navigationBarTitleDisplayMode(.inline)
-        .navigationBarItems(leading: BackButton())
-        .navigationBarBackButtonHidden(true)
-        .toolbar {
-            LogoEnglish()
-        }
+        .padding()
     }
     
     private func contents() {
--- Soomswim/Soomswim/Key/PostingFooter.swift
+++ Soomswim/Soomswim/Key/PostingFooter.swift
@@ -8,14 +8,7 @@
 import SwiftUI
 
 struct PostingFooter: View {
-    @State private var offer: Bool = false
-    @State private var text: String = "여기에 글을 남겨주세요"
-    @State var nickname = ""
-    
-    private let name: String
-    init(name: String) {
-        self.name = name
-    }
+    @Binding var popUp: Bool
     
     var body: some View {
         ZStack{
@@ -23,7 +16,7 @@ struct PostingFooter: View {
                 Spacer()
                 // 댓글쓰기 버튼
                 Button(action: {
-                    self.offer = true
+                    self.popUp = true
                 }) {
                     Text("댓글쓰기")
                         .foregroundColor(Color.green)
@@ -34,58 +27,6 @@ struct PostingFooter: View {
                 }
                 .padding(.all, 10)
                 .padding(.trailing)
-            }
-            if self.offer {
-                ZStack {
-                    Rectangle()
-                        .shadow(color: Color.gray, radius: 5)
-                        .foregroundColor(Color.white)
-                        .frame(width: 300, height: 500, alignment: .center)
-                    VStack {
-                        HStack {
-                            Spacer()
-                            Button(action: {
-                                // 수정필요: back-end 붙이기
-                                print("댓글 작성 clicked")
-                            }) {
-                                Text("보내기")
-                                    .foregroundColor(Color.blue)
-                            }
-                            Button(action: {
-                                self.offer = false
-                            }) {
-                                Text("취소")
-                                    .foregroundColor(Color.black)
-                            .padding(.all, 20)
-                            }
-                        }
-                        // 수정필요: 게시글 작성자 name 넣어야 함
-                        Text("To.\(name)")
-                            .font(.system(size: 21, weight: .heavy))
-                            .frame(width: 250, height: nil, alignment: .leading)
-                        
-                        TextEditor(text: self.$text)
-                            .foregroundColor(self.text == "여기에 글을 남겨주세요" ? .gray : .primary)
-                            .onTapGesture {
-                                if self.text == "여기에 글을 남겨주세요" {
-                                    self.text = ""
-                                }
-                                    
-                            }
-                            .disableAutocorrection(true)
-                            .padding()
-                        HStack() {
-                            Spacer()
-                            Text("From.")
-                            TextField("익명", text: $nickname)
-                        }
-                        .font(.system(size: 21, weight: .heavy))
-                        .padding(.all, 20)
-                        .padding(.leading, 120)
-                    }
-                }
-                .frame(width: 300, height: 500, alignment: .center)
-                .padding(.bottom, 50)
             }
         }
     }
